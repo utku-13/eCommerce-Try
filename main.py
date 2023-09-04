875537dced1c514edb50c27f9a8053b19a3db6b2
Utku Ozer <ozerutku13@gmail.com>
2023-09-04 03:13:18 +0300

    added functions for introduction of three brands. Added some cool pics.

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ def home():
 def ferrari():
     return render_template('ferrari.html')
 
+@app.route('/porsche')
+def porsche():
+    return render_template('porsche.html')
+
+@app.route('/bmw')
+def bmw():
+    return render_template('bmw.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
